b61ea3fe10b96102c702468a737c7ba78465dd6c
Artemii Kravchuk <mail@artiekra.org>
2026-07-09 23:36:57 +0300

    cors nits

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .auth.router import create_auth_router
 from .database import create_db_and_tables
@@ -14,6 +15,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Auth routes ---
 app.include_router(create_auth_router(), prefix="/auth", tags=["auth"])
